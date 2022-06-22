60e8ec464d80c53263425248f9405560cc241d32
Vladislav <v.sosinovich@itransition.com>
2022-06-22 11:29:40 +0200

    fixed test, deleted unused files

--- src/test/test.controller.spec.ts
+++ src/test/test.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { TestController } from './test.controller';
+import { TestService } from './test.service';
 
 describe('TestController', () => {
   let controller: TestController;
@@ -8,6 +9,7 @@ describe('TestController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TestController],
+      providers: [TestService],
     }).compile();
 
     controller = module.get<TestController>(TestController);
