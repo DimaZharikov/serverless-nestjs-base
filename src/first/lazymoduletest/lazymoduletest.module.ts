e1bdbf3646d1230ac065caf4e4202190d0f436b6
Dima.Zharikov <Dima.Zharikov@gmail.com>
2022-06-28 15:02:49 +0300

    clear up application of testing items

--- src/first/lazymoduletest/lazymoduletest.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { GlobalModule } from 'first/global/global.module';
-import { LazymoduletestService } from './lazymoduletest.service';
-
-@Module({
-  providers: [LazymoduletestService],
-  imports: [GlobalModule],
-})
-export class LazymoduletestModule {}
--- src/first/lazymoduletest/lazymoduletest.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { GlobalService } from 'first/global/global.service';
-
-@Injectable()
-export class LazymoduletestService {
-    constructor(public globalService: GlobalService){
-        console.log('LazymoduletestService instantiated');
-    }
-}
